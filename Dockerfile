a445122edbca33c39f98bb3b48fbed534ac920d0
Ayichew Hailu <ahailu@paperlesspost.com>
2017-05-18 13:59:25 -0400

    hello world running in container

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM golang:1.7
+
+COPY . /go/src/github.com/ahailu/go-helloworld
+WORKDIR /go/src/github.com/ahailu/go-helloworld
+
+RUN CGO_ENABLED=0 GOOS=$(uname -s | tr A-Z a-z) go install -ldflags "-X github.com/ahailu/go-helloworld/server.Revision=$(git rev-parse HEAD) -s" -a -installsuffix cgo github.com/ahailu/go-helloworld
+
+EXPOSE 5000
+
+CMD ["/go/bin/go-helloworld"]
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Hello world
+    fmt.Printf("hello, world\n")
+
+	//Variables
+	// var x string = "explicit"
+	// y := "inferred"
+	// fmt.Printf("explicit x: %s\n", x)
+	// fmt.Printf("inferred y: %s\n", y)
+	// var (
+	//   a = 5
+	//   b = 10
+	//   c = "hi"
+	// )
+	// fmt.Printf("inferred a: %d, b: %d, c: %s\n", a, b, c)
+	// fmt.Printf("formatting a: %s, b: %s, c: %s\n", a, b, c)
+
+}
